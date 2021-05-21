442a9c2c01c4c6510b5ec2f04ccfb14c7837e8a0
Cláudio Junior <>
2021-05-21 19:08:01 +0000

    organizando o código e testando a impressão, dificuldadesem reconhecer o maximo e o mínimo

--- main.cpp
+++ main.cpp
@@ -1,23 +1,33 @@
 #include <iostream>
 using namespace std;
 
+// FUNÇÃO COM OS PONTEIROS DA PRIMEIRA PARTE DA ATIVIDADE
+
 void ImprimeMaxEMin (){
 int *PonteiroInt, *PonteiroMax, *PonteiroMin;
   int Numeros[10], i, Max, Min;
   
   PonteiroInt = Numeros;
-  Max=Numeros[0];
-  for (int i=1; i<10; i++){
 
-  if (Numeros[i]>Numeros[i+1]){
+//INICIALIZANDO AS VARIÁVEIS DE MÁXIMO E MÍNIMO COM O PRIMEIRO ELEMENTO DO ARRAY
+
+  Max=Numeros[0];
+  Min=Numeros[0];
+
+//SEQUÊNCIA DE REPETICÃO PARA FAZER A LEITURA DO ARRAY E ENCONTRAR O MÁXIMO E O MÍNIMO
+
+  for (int i=0; i<10; i++){
+
+  if (Numeros[i]>Max){
       Max=Numeros[i];
       PonteiroMax= &Max;
-  }else {
+  }else if (Numeros[i]<Min) {
        Min=Numeros[i];
       PonteiroMin= &Min;
 
      }
    }
+//ÁREA DE IMPRESSÃO DOS RESULTADOS ENCONTRADOS
 
    cout<<"Endereço do vetor: "<< PonteiroInt <<endl;
    cout<<"Endereço do Maximo: "<< PonteiroMax <<endl;
@@ -26,19 +36,23 @@ int *PonteiroInt, *PonteiroMax, *PonteiroMin;
    cout<<"O maior número neste vetor é: "<< Max <<endl;
    cout<<"O menor número neste vetor é: "<< Min <<endl;
   
+  
    }
   
  
-
+//FUNÇÃO PRINCIPAL
 
 int main() {
 
    int Numeros[10], i;
 
+//SOLICITAÇÃO DOS DADOS AO USUÁRIO
+
   cout<< "digite um conjunto de números: "<<endl;
   cin>>Numeros[i];
   cout<< "vetor digitado:  " << Numeros[i]<<endl;
 
+//CHAMANDO A FUNÇÃO PARA ATIVAR OS PARÂMETROS SOLICITADOS NA ATIVIDADE 2
 
   ImprimeMaxEMin();
 }
