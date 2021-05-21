30731c6fb402c08601bccdeb0acabc8e9fc61ca2
Cláudio Junior <>
2021-05-21 18:31:32 +0000

    testando meu raciocinio do maior e menor, fora da função, pra checar o que está sendo impresso

--- main.cpp
+++ main.cpp
@@ -3,14 +3,28 @@ using namespace std;
 
 void ImprimeMaxEMin (){
 
-  int *PonteiroInt, *PonteiroMax, *PonteiroMin;
-  int Numeros[0], i, Max, Min;
   
-  *PonteiroInt = Numeros[i];
+   }
+  
+ 
+
+
+int main() {
+
+  int *PonteiroInt, *PonteiroMax, *PonteiroMin;
+  int Numeros[0], i, Max=Numeros[0], Min=Numeros[0];
+
+  cout<< "digite um conjunto de números: "<<endl;
+  cin>>Numeros[i];
+  cout<< "vetor digitado:  " << Numeros[i]<<endl;
+  
+ 
+  
+  PonteiroInt = Numeros;
  
   for (int i=0; i<10; i++){
 
-  if (Numeros[i]>Max){
+  if (Numeros[i]>Numeros[i+1]){
       Max=Numeros[i];
       PonteiroMax= &Max;
   }else {
@@ -19,19 +33,12 @@ void ImprimeMaxEMin (){
 
      }
    }
-  
- }
 
+   cout<<"Endereço do vetor: "<< PonteiroInt <<endl;
+   cout<<"Endereço do Maximo: "<< PonteiroMax <<endl;
+   cout<<"Endereço do Minimo: "<< PonteiroMin <<endl;
 
-int main() {
-
-  int Numeros[0], i;
-  
-
-  cout<< "digite um conjunto de números: "<<endl;
-  cin>>Numeros[i];
-  cout<< "vetor digitado:  " << Numeros[i]<<endl;
-  
-  ImprimeMaxEMin();
+   cout<<"O maior número neste vetor é: "<< Max <<endl;
+   cout<<"O menor número neste vetor é: "<< Min <<endl;
 }
 
