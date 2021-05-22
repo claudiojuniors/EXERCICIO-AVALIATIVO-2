7e70c0863f8b2134f169fdd408f1b921b9f6c9e2
Cláudio Junior <>
2021-05-22 03:00:56 +0000

    corrigi a função agora está saindo o valor de máximo e mínimo

--- main.cpp
+++ main.cpp
@@ -5,68 +5,61 @@ using namespace std;
 
 // FUNÇÃO COM OS PONTEIROS DA PRIMEIRA PARTE DA ATIVIDADE
 
+int Numeros[10];
+
 void ImprimeMaxEMin (int &Max, int &Min){
-int *PonteiroMax, *PonteiroMin;
-  int i ;
-  int  Numeros[10]={}, *PonteiroInt;
 
-  PonteiroInt = Numeros;
+    int *PonteiroMax, *PonteiroMin, *PonteiroInt;
 
-//INICIALIZANDO AS VARIÁVEIS DE MÁXIMO E MÍNIMO COM O PRIMEIRO ELEMENTO DO ARRAY
+    PonteiroInt = Numeros;
 
-  Max=Numeros[0];
-  Min=Numeros[0];
+    //INICIALIZANDO AS VARIÁVEIS DE MÁXIMO E MÍNIMO COM O PRIMEIRO ELEMENTO DO ARRAY
 
-//SEQUÊNCIA DE REPETICÃO PARA FAZER A LEITURA DO ARRAY E ENCONTRAR O MÁXIMO E O MÍNIMO
+    Max=Numeros[0];
+    Min=Numeros[0];
 
-  for (int i=0; i<10; i++){
+    //SEQUÊNCIA DE REPETICÃO PARA FAZER A LEITURA DO ARRAY E ENCONTRAR O MÁXIMO E O MÍNIMO
 
-  if (Numeros[i]>Max){
-      Max=Numeros[i];
-      PonteiroMax= &Max;
-      
-      
-  }else  {
-       Min=Numeros[i];
-      PonteiroMin= &Min;
+    for (int i=0; i<10; i++){
+        if (Numeros[i] > Max){
+            Max=Numeros[i];
+        }if (Numeros[i] < Min){
+            Min=Numeros[i];
+        }
+    }
+    PonteiroMax = &Max;
+    PonteiroMin = &Min;
 
-     }
-   }
-  
-//ÁREA DE IMPRESSÃO DOS RESULTADOS ENCONTRADOS
+    //ÁREA DE IMPRESSÃO DOS RESULTADOS ENCONTRADOS
 
-   cout<<"Endereço do vetor: "<< PonteiroInt <<endl;
-   cout<<"Endereço do Maximo: "<< PonteiroMax <<endl;
-   cout<<"Endereço do Minimo: "<< PonteiroMin <<endl;
-  
+    cout<<"Endereco do vetor: "<< PonteiroInt <<endl;
+    cout<<"Endereco do Maximo: "<< PonteiroMax <<endl;
+    cout<<"Endereco do Minimo: "<< PonteiroMin <<endl;
+    cout<<"O maior numero neste vetor eh: "<< Max <<endl;
+    cout<<"O menor numero neste vetor eh: "<< Min <<endl;
+
 }
-  
- 
+
+
 //FUNÇÃO PRINCIPAL
 
 int main() {
 
-   int Numeros[10], i, Maior, Menor;
+   int Maior, Menor;
 
-//SOLICITAÇÃO DOS DADOS AO USUÁRIO
-for (int i=0; i<10; i++){
-  cout<< "digite um número: "<<endl;
-  cin>>Numeros[i];  
-}
+    //SOLICITAÇÃO DOS DADOS AO USUÁRIO
+    for (int i=0; i<10; i++){
+      cout<< "digite um numero: " << endl;
+      cin>>Numeros[i];
+    }
 
-for (int i=0; i<10; i++){
-cout<< "posição: " << i << "= "<< Numeros[i]<<endl;
-}
+    for (int i=0; i<10; i++){
+        cout<< "posicao: " << i << " = "<< Numeros[i]<<endl;
+    }
 
-//CHAMANDO A FUNÇÃO PARA ATIVAR OS PARÂMETROS SOLICITADOS NA ATIVIDADE 2
+    //CHAMANDO A FUNÇÃO PARA ATIVAR OS PARÂMETROS SOLICITADOS NA ATIVIDADE 2
 
-//ImprimeMaxEMin(Max, Min);
-
-
- ImprimeMaxEMin( Maior ,Menor);
-
-cout<<"O maior número neste vetor é: "<< Maior <<endl;
-cout<<"O menor número neste vetor é: "<< Menor <<endl;
+    ImprimeMaxEMin(Maior, Menor);
 
 }
 
